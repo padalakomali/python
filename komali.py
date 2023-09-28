aa63bbe656c63370907db4fc167f289bb3a91dd5
padala komali <144433345+padalakomali@users.noreply.github.com>
2023-09-28 10:10:40 +0530

    Update komali.py

--- komali.py
+++ komali.py
@@ -11,3 +11,4 @@ print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 sum =num1 +num2
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
